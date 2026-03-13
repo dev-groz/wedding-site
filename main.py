e5226bbc4358c3aa1c1dc21ded95611c554be02c
dev-groz <kirill@MacBook-Air-Kirill.local>
2026-03-13 23:23:44 +0700

    Add static html

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
+from fastapi.responses import FileResponse
 
 app = FastAPI()
 
 @app.get('/')
-async def hello():
-    return {'hello':'world'}
+async def home(request: Request):
+    return FileResponse('templates/index.html')
