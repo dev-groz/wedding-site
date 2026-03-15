77a4ca8970ed0028cef10960abab312865531628
dev-groz <kirill@MacBook-Air-Kirill.local>
2026-03-16 00:14:29 +0700

    add health endpoint

--- main.py
+++ main.py
@@ -1,8 +1,16 @@
 from fastapi import FastAPI, Request
 from fastapi.responses import FileResponse
 
-app = FastAPI()
+app = FastAPI(
+    redoc_url=None,
+    docs_url=None,
+    openapi_url=None,
+    )
 
 @app.get('/')
 async def home(request: Request):
     return FileResponse('templates/index.html')
+
+@app.get('/health')
+async def health():
+    return {'status':'ok'}
