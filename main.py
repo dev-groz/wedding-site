4d9d82603d6440420b6ab343ad46c47440db11f5
dev-groz <kirill@MacBook-Air-Kirill.local>
2026-03-28 00:57:41 +0700

    add placeholder for send_info route

--- main.py
+++ main.py
@@ -11,6 +11,11 @@ app = FastAPI(
 async def home(request: Request):
     return FileResponse('templates/index.html')
 
+@app.get('/send_info')
+async def send_info(first_name: str, last_name: str, phone: str):
+    print({'first_name': first_name, 'last_name': last_name, 'phone': phone})
+    return "ok"
+
 @app.head('/health')
 @app.get('/health')
 async def health():
